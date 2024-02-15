7bd1a2d4f71b6e0b0b974e91c92c73ec2c3569d5
ffalconet <f_falconet@yahoo.fr>
2024-02-15 11:58:28 +0100

    Adding Theme

--- paymentComponents.js
+++ paymentComponents.js
@@ -1,11 +1,229 @@
+/* global CheckoutWebComponents */
+const themes = {
+  default: {},
+  terminal: {
+    // Background Color
+    colorBackgroundPrimary: '#17201E',
+    colorBackgroundDisabled: '#f7f7f7',
+    colorBackgroundError: '#fff3f5',
+    colorBackgroundActionPrimary: '#00CC2D',
+    colorBackgroundActionDisabled: '#f0f0f0',
+
+    // Text Color
+    colorTextPrimary: '#00CC2D',
+    colorTextDisabled: '#717171',
+    colorTextError: '#FF90A3',
+    colorTextActionPrimary: '#17201E',
+    colorTextActionSecondary: '#00CC2D',
+
+    // Border Color
+    colorBorderPrimary: '#8a8a8a',
+    colorBorderSecondary: '#d9d9d9',
+    colorBorderFocus: '#00CC2D',
+    colorOutlineFocus: '#00CC2D',
+    colorBorderError: '#FF90A3',
+
+    // Icon Color
+    colorIconPrimary: '#00CC2D',
+    colorIconDisabled: '#8a8a8a',
+    colorIconAction: '#717171',
+    colorIconError: '#FF90A3',
+  },
+  runway: {
+    title: {
+      fontFamily: '"Times New Roman", serif',
+      fontSize: '18px',
+      lineHeight: '20px',
+      fontWeight: 500,
+      letterSpacing: 0,
+    },
+    subheading: {
+      fontFamily: '"Times New Roman", serif',
+      fontSize: '15px',
+      lineHeight: '20px',
+      fontWeight: 500,
+      letterSpacing: 0,
+    },
+    body: {
+      fontFamily: '"Times New Roman", serif',
+      fontSize: '15px',
+      lineHeight: '18px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+    footnote: {
+      fontFamily: '"Times New Roman", serif',
+      fontSize: '12px',
+      lineHeight: '18px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+    button: {
+      fontFamily: '"Times New Roman", serif',
+      fontSize: '15px',
+      lineHeight: '20px',
+      fontWeight: 500,
+      letterSpacing: 0,
+    },
+    input: {
+      fontFamily: '"Times New Roman", serif',
+      fontSize: '15px',
+      lineHeight: '20px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+    label: {
+      fontFamily: '"Times New Roman", serif',
+      fontSize: '15px',
+      lineHeight: '20px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+
+    // Spacing
+    spacing: ['2px', '10px', '12px', '15px', '18px', '24px', '32px', '40px'],
+
+    // Border
+    borderRadius: ['6px', '12px'],
+    borderWidth: '1px',
+
+    // Background Color
+    colorBackgroundPrimary: '#FFFFFF',
+    colorBackgroundDisabled: '#f7f7f7',
+    colorBackgroundError: '#fff3f5',
+    colorBackgroundActionPrimary: '#0b5ff0',
+    colorBackgroundActionDisabled: '#f0f0f0',
+
+    // Text Color
+    colorTextPrimary: '#000000',
+    colorTextDisabled: '#727272',
+    colorTextError: '#ad283e',
+    colorTextActionPrimary: '#ffffff',
+    colorTextActionSecondary: '#0b5ff0',
+
+    // Border Color
+    colorBorderPrimary: '#8a8a8a',
+    colorBorderSecondary: '#d9d9d9',
+    colorBorderFocus: '#0b5ff0',
+    colorOutlineFocus: 'rgba(11, 95, 240, 0.5)',
+    colorBorderError: '#ad283e',
+
+    // Icon Color
+    colorIconPrimary: '#000000',
+    colorIconDisabled: '#8a8a8a',
+    colorIconAction: '#0b5ff0',
+    colorIconError: '#ad283e',
+  },
+  clouds: {
+    title: {
+      fontFamily: 'cursive',
+      fontSize: '18px',
+      lineHeight: '20px',
+      fontWeight: 500,
+      letterSpacing: 0,
+    },
+    subheading: {
+      fontFamily: 'cursive',
+      fontSize: '15px',
+      lineHeight: '20px',
+      fontWeight: 500,
+      letterSpacing: 0,
+    },
+    body: {
+      fontFamily: 'cursive',
+      fontSize: '15px',
+      lineHeight: '18px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+    footnote: {
+      fontFamily: 'cursive',
+      fontSize: '12px',
+      lineHeight: '18px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+    button: {
+      fontFamily: 'cursive',
+      fontSize: '30px',
+      lineHeight: '20px',
+      fontWeight: 500,
+      letterSpacing: 0,
+    },
+    input: {
+      fontFamily: 'cursive',
+      fontSize: '15px',
+      lineHeight: '20px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+    label: {
+      fontFamily: 'cursive',
+      fontSize: '15px',
+      lineHeight: '20px',
+      fontWeight: 400,
+      letterSpacing: 0,
+    },
+
+    // Background Color
+    colorBackgroundPrimary: '#FBF6FF',
+    colorBackgroundDisabled: '#f7f7f7',
+    colorBackgroundError: '#fff3f5',
+    colorBackgroundActionPrimary: '##a096d7',
+    colorBackgroundActionDisabled: '#f0f0f0',
+
+    // Text Color
+    colorTextPrimary: '#461E67',
+    colorTextDisabled: '#AFAFAF',
+    colorTextError: '#E48181',
+    colorTextActionPrimary: '#FFFFFF',
+    colorTextActionSecondary: '#9A22FF',
+
+    // Border Color
+    colorBorderPrimary: '#E8CEF6',
+    colorBorderSecondary: '#AFAFAF',
+    colorBorderFocus: '#9A22FF',
+    colorOutlineFocus: '#9A22FF',
+    colorBorderError: '#FF90A3',
+
+    // Icon Color
+    colorIconPrimary: '#461E67',
+    colorIconDisabled: '#AFAFAF',
+    colorIconAction: '#9A22FF',
+    colorIconError: '#FF90A3',
+  },
+};
+
+
 var country = 'GB';
 var currency = 'GBP';
 var locale = 'en';
+var theme = themes.default;
 
 window.onload = function () {
-  createPaymentSession(country, currency, locale);
+  createPaymentSession(country, currency, locale, theme);
 };
 
+
+$("#theme-select").change(function () {
+  switch(this.value) {
+    case 'default':
+      theme = themes.default
+      break;
+    case 'clouds':
+      theme = themes.clouds
+      break;
+    case 'terminal':
+      theme = themes.terminal
+      break;
+    case 'runway':
+      theme = themes.runway
+      break;
+    default:
+        theme = themes.default
+  }
+  createPaymentSession(country, currency, locale, theme);
+});
 
 
 $("#customer-location-select").change(function () {
@@ -61,10 +279,10 @@ $("#customer-location-select").change(function () {
       locale = 'en';
       // code block
   }
-  createPaymentSession(country, currency, locale);
+  createPaymentSession(country, currency, locale, theme);
 });
 
-async function createPaymentSession(country, currency, locale) {
+async function createPaymentSession(country, currency, locale, theme) {
 
   $("#successful-payment-message").hide();
 
@@ -89,11 +307,12 @@ async function createPaymentSession(country, currency, locale) {
   };
 
   $.ajax(settings).done(async function (response) {
-    console.log(response);
+
     const checkoutWebComponents = await CheckoutWebComponents({
       publicKey: publicKey,
       environment: "sandbox",
       locale: locale,
+      appearance: theme,
       paymentSession: response,
       onReady: () => {
         console.log("onReady");
@@ -127,7 +346,6 @@ async function createPaymentSession(country, currency, locale) {
     });
 
     const payments = checkoutWebComponents.create("payments");
-
     payments.mount(document.getElementById("payments"));
   });
 }
